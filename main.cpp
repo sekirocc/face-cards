a1b361e19e75e23524ebd7906cbec1f129f19d2e
chenjie.101 <chenjie.101@bytedance.com>
2023-09-14 23:44:17 +0800

    integrate DondeToolkits

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "video_context.h"
 #include "picture_factory.h"
+#include "donde/feature_extract/face_pipeline.h"
 
 #include <QApplication>
 
@@ -11,6 +12,36 @@ int main(int argc, char *argv[])
     VideoContext video_ctx{"/tmp/Iron_Man-Trailer_HD.mp4"};
     MediaController controller{video_ctx};
     PictureFactory factory{video_ctx};
+
+    json conf = R"(
+    {
+        "detector": {
+          "concurrent": 1,
+          "device_id": "CPU",
+          "model": "./contrib/models/face-detection-adas-0001.xml",
+          "warmup": false
+        },
+        "landmarks": {
+          "concurrent": 1,
+          "device_id": "CPU",
+          "model": "./contrib/models/facial-landmarks-35-adas-0002.xml",
+          "warmup": false
+        },
+        "aligner": {
+          "concurrent": 1,
+          "device_id": "CPU",
+          "warmup": false
+        },
+        "feature": {
+          "concurrent": 1,
+          "device_id": "CPU",
+          "model": "./contrib/models/Sphereface.xml",
+          "warmup": false
+        }
+    }
+)"_json;
+
+    donde_toolkits::feature_extract::FacePipeline pipeline{conf};
 
     MainWindow w{factory, controller};
     w.show();
